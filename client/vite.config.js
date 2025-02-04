dbdb86b17e759dc19309c39d5de0fefac2ef0f4f
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2025-02-05 00:48:39 +0530

    config updted

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,7 +3,17 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"], // Separate React-related libraries
+          vendor: ["lodash", "axios"], // Other common dependencies
+        },
+      },
+    },
+    chunkSizeWarningLimit: 1000, // Increase limit (optional)
+  },
   server: {
     port:10000,
     host: '0.0.0.0', 
