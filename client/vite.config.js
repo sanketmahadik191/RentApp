e0d1f719832958f3a1642ba81d7c681aae008e00
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2025-02-05 00:35:59 +0530

    v5

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,8 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
 
   server: {
+    port:10000,
+    host: '0.0.0.0', 
     proxy: {
       '/api': {
         target: "https://rentapp-edwq.onrender.com",
