b2ff34f9194b5380e7a3ed0dcad2919ce83eb3ea
SANKET MAHADIK <125791466+sanketmahadik191@users.noreply.github.com>
2025-02-05 01:53:43 +0530

    config10

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('development'), // Force React to run in development mode
+  },
   mode: "development",
   build: {
     // Set production build without dead code elimination for development mode
